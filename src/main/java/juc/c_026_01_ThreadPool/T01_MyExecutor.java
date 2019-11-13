efe3472cda29bbda647206d22a70f07dd6d69edf
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-13 23:59:43 +0800

    1.面试题 交替打印输出 2.普通线程池介绍及简单使用

---
+++ src/main/java/juc/c_026_01_ThreadPool/T01_MyExecutor.java
@@ -0,0 +1,12 @@
+package juc.c_026_01_ThreadPool;
+
+import java.util.concurrent.Executor;
+
+public class T01_MyExecutor implements Executor {
+
+    @Override
+    public void execute(Runnable command) {
+        //new Thread(command).run();
+        command.run();
+    }
+}
